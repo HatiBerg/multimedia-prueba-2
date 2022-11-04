db9bf47d58810d99b584c106e0b189e32115c1a6
Esteban Adolfo Ponce Lira <esteban.ponce@alumnos.upla.cl>
2022-11-04 15:46:13 -0300

    diseño de página

---
+++ pagina1.php
@@ -0,0 +1,57 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página 1</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container text-center" style="background-color:#c7c7c7;">
+
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid text-center">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mx-auto">
+                        <div class="navbar-item">
+                            <a class="navbar-brand">Menú de Navegación:</a>
+                        </div>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Ingresar Persona</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Lista Personas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Estadísticas</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="row">
+            <div class="col-8">
+                <h1>hola</h1>
+            </div>
+            <div class="col-4 ms-auto">
+                <h1>hola</h1>
+            </div>
+            <div class="w-100"></div>
+            <div class="col">
+                <h1>hola</h1>
+            </div>
+        </div>
+
+
+    </div>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
